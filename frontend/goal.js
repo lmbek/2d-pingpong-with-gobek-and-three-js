e37553d9f25c8bd38b658e5eeea5e4f8f44d010a
IdaDania <54858@student.eadania.dk>
2023-01-05 20:58:50 +0100

    goal.js: edited * added mesh and boundingMesh * added constructor that takes one argument, the xPosition * the position of the goal is set through the game.js file at setup

--- frontend/goal.js
+++ frontend/goal.js
@@ -1,5 +1,21 @@
-class Goal {
+class Goal extends THREE.Group {
+    mesh = new THREE.Mesh(new THREE.BoxGeometry(0, window.innerHeight, 0));
+    boundingMesh = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3())
+    xPosition = 0
 
+    constructor(xPosition) {
+        super()
+        this.xPosition = xPosition
+        this.setPosition()
 
+        // add mesh
+        this.add(this.mesh)
+        this.boundingMesh.setFromObject(this.mesh)
+    }
 
+    setPosition() {
+        // mesh z position
+        this.mesh.position.z = 0
+        this.mesh.position.x = this.xPosition
+    }
 }
