cfd09d3baf60e15986d8a2754cf58eafa769100b
Lars Morten Bek <lars@beksoft.dk>
2023-07-11 10:24:24 +0200

    Upgraded to gobek 0.6.3

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"github.com/NineNineFive/go-local-web-gui/fileserver"
-	"github.com/NineNineFive/go-local-web-gui/launcher"
-	"net/http"
+	"fmt"
+	"github.com/lmbek/gobek/launcher"
 	"os"
-	"runtime"
 )
 
-// For windows we need a organisation name and project name
+// For windows, we need a organisation name and project name
 var organisationName = "Development" // put in organisation name
 var projectName = "PingPong"         // put in project name
 
@@ -16,51 +14,19 @@ var frontendPath = "./frontend" // this should be set to where frontend files is
 
 // remember to change the ports to something unique
 var chromeLauncher = launcher.ChromeLauncher{
-	Location:                "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-	LocationCMD:             "C:\\\"Program Files\"\\Google\\Chrome\\Application\\chrome.exe",
+	Location:                os.Getenv("programfiles") + "\\Google\\Chrome\\Application\\chrome.exe",
 	FrontendInstallLocation: os.Getenv("localappdata") + "\\Google\\Chrome\\InstalledApps\\" + organisationName + "\\" + projectName,
-	Domain:                  "localhost",
-	PortMin:                 35000,
-	PreferredPort:           35050,
-	PortMax:                 35100,
 }
 
-var chromiumLauncher = launcher.ChromiumLauncher{
-	Location:      "/var/lib/snapd/desktop/applications/chromium_chromium.desktop", // TODO: check if better location or can be customised
-	Domain:        "localhost",
-	PortMin:       35000,
-	PreferredPort: 35050,
-	PortMax:       35100,
-}
+var chromiumLauncher = launcher.DefaultChromiumLauncher
 
 func main() {
 	launchApp()
 }
 
-func initHTTPHandlers() {
-	// static fileserver
-	http.HandleFunc("/", fileserver.ServeFileServer)
-
-	// api (local api can be added)
-	//http.HandleFunc("/api/", api.ServeAPIUseGZip)
-}
-
 func launchApp() {
-	switch runtime.GOOS {
-	case "windows":
-		initHTTPHandlers()
-		launcher.StartOnWindows(frontendPath, chromeLauncher)
-		return
-	case "darwin": // "mac"
-		panic("Darwin/Mac Will Not Be Supported")
-		return
-	case "linux": // "linux"
-		initHTTPHandlers()
-		launcher.StartOnLinux(frontendPath, chromiumLauncher)
-		return
-	default: // "freebsd", "openbsd", "netbsd"
-		initHTTPHandlers()
-		launcher.StartOnLinux(frontendPath, chromiumLauncher)
-		return
+	err := launcher.StartDefault(frontendPath, chromeLauncher, chromiumLauncher)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
